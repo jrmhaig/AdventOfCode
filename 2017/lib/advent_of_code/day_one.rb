fd66a17028d1f4d77f34ba921cfe0bdc544b91ba
Joe Haig <joseph.haig@arnoldclark.com>
2017-12-01 10:16:54 +0000

    Day one part two

--- 2017/lib/advent_of_code/day_one.rb
+++ 2017/lib/advent_of_code/day_one.rb
@@ -1,10 +1,20 @@
 module AdventOfCode
   module DayOne
-    def self.captcha code
+    def self.captcha1 code
+      self.captcha code, 1
+    end
+
+    def self.captcha2 code
+      self.captcha code, code.length / 2
+    end
+
+    private
+
+    def self.captcha code, rotation
       digits = code.split(//).map(&:to_i)
       sum = 0
       digits.each_with_index do |n, i|
-        sum += n if digits[(i+1) % digits.length] == n
+        sum += n if digits[(i + rotation) % digits.length] == n
       end
       sum
     end
--- 2017/spec/day_one_spec.rb
+++ 2017/spec/day_one_spec.rb
@@ -1,38 +1,62 @@
 require 'advent_of_code/day_one'
 
 RSpec.describe AdventOfCode::DayOne do
-  describe '#captcha' do
+  describe '#captcha1' do
     it 'returns zero for no repeated digits' do
-      expect(AdventOfCode::DayOne.captcha '1234').to eq 0
+      expect(AdventOfCode::DayOne.captcha1 '1234').to eq 0
     end
 
     it 'detects a repeated digit' do
-      expect(AdventOfCode::DayOne.captcha '112').to eq 1
+      expect(AdventOfCode::DayOne.captcha1 '112').to eq 1
     end
 
     it 'sums the repeated digit' do
-      expect(AdventOfCode::DayOne.captcha '332').to eq 3
+      expect(AdventOfCode::DayOne.captcha1 '332').to eq 3
     end
 
     it 'matches last against first' do
-      expect(AdventOfCode::DayOne.captcha '52135').to eq 5
+      expect(AdventOfCode::DayOne.captcha1 '52135').to eq 5
     end
 
     context 'Examples' do
       it 'satisfies the first given example' do
-        expect(AdventOfCode::DayOne.captcha '1122').to eq 3
+        expect(AdventOfCode::DayOne.captcha1 '1122').to eq 3
       end
 
       it 'satisfies the second given example' do
-        expect(AdventOfCode::DayOne.captcha '1111').to eq 4
+        expect(AdventOfCode::DayOne.captcha1 '1111').to eq 4
       end
 
       it 'satisfies the third given example' do
-        expect(AdventOfCode::DayOne.captcha '1234').to eq 0
+        expect(AdventOfCode::DayOne.captcha1 '1234').to eq 0
       end
 
       it 'satisfies the fourth given example' do
-        expect(AdventOfCode::DayOne.captcha '91212129').to eq 9
+        expect(AdventOfCode::DayOne.captcha1 '91212129').to eq 9
+      end
+    end
+  end
+
+  describe '#captcha2' do
+    context 'Examples' do
+      it 'satisfies the first given example' do
+        expect(AdventOfCode::DayOne.captcha2 '1212').to eq 6
+      end
+
+      it 'satisfies the second given example' do
+        expect(AdventOfCode::DayOne.captcha2 '1221').to eq 0
+      end
+
+      it 'satisfies the third given example' do
+        expect(AdventOfCode::DayOne.captcha2 '123425').to eq 4
+      end
+
+      it 'satisfies the fourth given example' do
+        expect(AdventOfCode::DayOne.captcha2 '123123').to eq 12
+      end
+
+      it 'satisfies the fifth given example' do
+        expect(AdventOfCode::DayOne.captcha2 '12131415').to eq 4
       end
     end
   end
