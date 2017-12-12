55a3918c75ce0b514db37d7c697993842aaeb7b4
Joe Haig <joseph.haig@arnoldclark.com>
2017-12-12 17:20:58 +0000

    Day twelve part two

--- 2017/day_twelve.rb
+++ 2017/day_twelve.rb
@@ -7,3 +7,13 @@ data = File.read('day_twelve_input.txt').chomp.split(/\n/)
 comms = AdventOfCode::DayTwelve.new *data
 
 puts "Part one: #{comms.group(0).count}"
+
+found = []
+count = 0
+2000.times do |i|
+  next if found.include? i
+  found += comms.group(i)
+  count += 1
+end
+
+puts "Part two: #{count}"
