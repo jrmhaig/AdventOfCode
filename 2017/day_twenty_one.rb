2d88c5d5279ec9ff09a785b183d6e01c3f323290
Joseph Haig <josephhaig@gmail.com>
2017-12-22 16:56:55 +0000

    Day twenty one part two

--- 2017/day_twenty_one.rb
+++ 2017/day_twenty_one.rb
@@ -7,3 +7,8 @@ rules = File.read('day_twenty_one_input.txt').chomp.split(/\n/)
 grid = AdventOfCode::DayTwentyOne.new *rules
 5.times { grid.step }
 puts "Part one: #{grid.pattern.each.sum}"
+(18-5).times { |i|
+  puts i
+  grid.step
+}
+puts "Part two: #{grid.pattern.each.sum}"
