2bb476f352eacc22d68e7da154fe8dcca73a64f2
Joe Haig <joseph.haig@arnoldclark.com>
2017-12-22 14:38:05 +0000

    Day twenty two part two

--- 2017/day_twenty_two.rb
+++ 2017/day_twenty_two.rb
@@ -7,3 +7,8 @@ data = File.read('day_twenty_two_input.txt').chomp
 grid = AdventOfCode::DayTwentyTwo.new data
 10000.times { grid.step }
 puts "Part one: #{grid.infection_count}"
+
+grid = AdventOfCode::DayTwentyTwo.new data
+grid.evolved = true
+10000000.times { grid.step }
+puts "Part two: #{grid.infection_count}"
--- 2017/lib/advent_of_code/day_twenty_two.rb
+++ 2017/lib/advent_of_code/day_twenty_two.rb
@@ -1,37 +1,80 @@
-require 'matrix'
-
 module AdventOfCode
   class DayTwentyTwo
-    attr_reader :position, :direction, :infected, :infection_count
+    attr_reader :position, :direction, :infection_count,
+                :weakened, :infected, :flagged
+    attr_writer :evolved
+
+    RIGHT = {
+      [0, 1] => [1, 0],
+      [1, 0] => [0, -1],
+      [0, -1] => [-1, 0],
+      [-1, 0] => [0, 1]
+    }
+    LEFT = {
+      [0, 1] => [-1, 0],
+      [-1, 0] => [0, -1],
+      [0, -1] => [1, 0],
+      [1, 0] => [0, 1]
+    }
+    REVERSE = {
+      [0, 1] => [0, -1],
+      [1, 0] => [-1, 0],
+      [0, -1] => [0, 1],
+      [-1, 0] => [1, 0]
+    }
 
     def initialize start
-      @position = Vector[0, 0]
-      @direction = Vector[0, 1]
+      @position = [0, 0]
+      @direction = [0, 1]
+      @weakened = []
       @infected = []
-      @right = Matrix[[0, 1], [-1, 0]]
-      @left = Matrix[[0, -1], [1, 0]]
+      @flagged = []
+      @bits = {}
       @infection_count = 0
+      @evolved = false
 
       rows = start.split(/\n/)
       offset = (rows.count - 1) / 2
       rows.each_with_index do |row, i|
         j = -1
         while (j = row.index('#', j+1))
-          @infected << Vector[j - offset, offset - i]
+          @infected << [j - offset, offset - i]
+          set_bit(j - offset, offset - i, :infected)
         end
       end
     end
 
     def step
-      if @infected.include? @position
-        @direction = @right * @direction
-        @infected -= [@position]
-      else
+      if get_bit(*@position) == :flagged
+        @direction = REVERSE[@direction]
+        set_bit *@position, nil
+      elsif get_bit(*@position) == :infected
+        @direction = RIGHT[@direction]
+        set_bit *@position, (@evolved ? :flagged : nil)
+      elsif get_bit(*@position) == :weakened
+        set_bit *@position, :infected
         @infection_count += 1
-        @direction = @left * @direction
-        @infected  << @position
+      else
+        @direction = LEFT[@direction]
+        set_bit *@position, (@evolved ? :weakened : :infected)
+        @infection_count += 1 unless @evolved
       end
-      @position += @direction
+      @position = [
+        @position[0] + @direction[0],
+        @position[1] + @direction[1]
+      ]
+    end
+
+    def get_bit x, y
+      return nil if @bits[x].nil?
+      @bits[x][y]
+    end
+
+    private
+
+    def set_bit x, y, value = nil
+      @bits[x] = {} if @bits[x].nil?
+      @bits[x][y] = value
     end
   end
 end
