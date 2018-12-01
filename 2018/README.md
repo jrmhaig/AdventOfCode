c0d51e89771f17d33b4a3e1938ce2a45db103df8
Joseph Haig <josephhaig@gmail.com>
2018-12-01 11:20:54 +0000

    Time runs

---
+++ 2018/README.md
@@ -0,0 +1,6 @@
+# Timings
+
+## Day One
+
+* Part one: < 1 seconds
+* Part two (naive algorithm): 55 seconds
--- 2018/day_one.rb
+++ 2018/day_one.rb
@@ -1,6 +1,14 @@
+#!/usr/bin/env ruby
+
 changes = File.read('day_one_input.txt').split(/\n/).map(&:to_i)
 
-puts "Part one: #{changes.sum}"
+t0 = Time.now
+counter = 0
+steps = changes.map do |i|
+  counter += i
+end
+t1 = Time.now
+puts "Part one: #{steps[-1]}"
 
 collection = [0]
 while collection.count(collection[-1]) <= 1
@@ -8,4 +16,8 @@ while collection.count(collection[-1]) <= 1
   collection << collection[-1] + i
   changes << i
 end
+t2 = Time.now
 puts "Part two: #{collection[-1]}"
+
+puts "Part one time: #{t1-t0} seconds"
+puts "Part two time: #{t2-t1} seconds"
