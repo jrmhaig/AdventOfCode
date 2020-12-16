bb1d8d491bca669fb66222def4b0c156e5587980
Joe Haig <josephhaig@gmail.com>
2020-12-16 21:48:24 +0000

    2020 day 16 part 2

--- 2020/day_sixteen.rb
+++ 2020/day_sixteen.rb
@@ -1,6 +1,8 @@
 #!/bin/env ruby
 
 class Rule
+  attr_reader :name
+
   def initialize line
     @name, range_string = line.split(':')
     @bounds = range_string.gsub(/\s+/, '').split(/[^\d]+/).map(&:to_i).each_slice(2)
@@ -13,26 +15,55 @@ class Rule
 end
 
 class Ticket
+  attr_reader :numbers
+
   def initialize numbers:, rules:
     @numbers = numbers.split("\n").last.split(',').map(&:to_i)
     @rules = rules
   end
 
   def find_invalid
-    @numbers.reject { |n| check_rules n }
+    @numbers.reject { |n| possible_rules_for(n).any? }
+  end
+
+  def valid?
+    find_invalid.empty?
+  end
+
+  def possible_rules
+    @numbers.map { |n| possible_rules_for n }
   end
 
   private
-  def check_rules n
-    @rules.map { |rule| rule.valid? n }.lazy.any?(true)
+  def possible_rules_for n
+    @rules.map { |rule| rule.valid?(n) ? rule : nil }.compact
   end
 end
 
-rules, my_ticket, other_tickets = File.read('day_sixteen_input.txt').chomp.split("\n\n")
-#rules, my_ticket, other_tickets = File.read('day_sixteen_test_input.txt').chomp.split("\n\n")
+rules, my_ticket_line, other_tickets = File.read('day_sixteen_input.txt').chomp.split("\n\n")
 
 rules = rules.split("\n").map! { |rule| Rule.new rule }
 tickets = other_tickets.split("\n")[1..].map do |ticket|
   Ticket.new numbers: ticket, rules: rules
 end
 puts "1) #{tickets.map { |ticket| ticket.find_invalid.sum }.sum}"
+
+valid_tickets = tickets.select {  |ticket| ticket.valid? }
+
+my_ticket = Ticket.new(numbers: my_ticket_line, rules: rules)
+
+possible = my_ticket.possible_rules
+valid_tickets.each do |ticket|
+  possible = possible.zip(ticket.possible_rules).map { |a, b| a & b }
+end
+
+while possible.map(&:count).max > 1
+  singles = possible.select { |rs| rs.count == 1 }.flatten
+  possible.map! do |rs|
+    rs.count == 1 ? rs : rs - singles
+  end
+end
+
+puts "2) #{my_ticket.numbers.map.with_index do |n, i|
+  possible[i].first.name =~ /^departure/ ? n : 1
+end.inject(&:*)}"
